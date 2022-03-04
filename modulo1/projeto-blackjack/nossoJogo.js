37bfce37176cb89751d5d7f013fb97ceffffc173
Amanda Maria <amanda.moov@gmail.com>
2022-03-04 17:26:13 -0300

    Alterando lógica do jogo

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -10,31 +10,34 @@
  * 
  * 
  */
-function criarJogador(qtdCartas) {
-   const jogador = []
+    console.log(`Boas vindas ao jogo de Blackjack!`);
 
-   for(let i = 0; i < qtdCartas; i ++) {
-      let carta = comprarCarta()
-      jogador.push(carta)
-   }
-
-   return jogador
-}
-
-console.log("Boas vindas ao jogo de Blackjack!");
-if(confirm("Quer iniciar uma nova rodada?")) {
-   const usuario = criarJogador(2);
-   const computador = criarJogador(2);
-
-   const pontuacaoCartasUsuario = usuario.map((carta, index, array) => {
-      carta.valor
-   })
-
-   const pontuacaoCartasComputador = usuario.map((carta, index, array) => {
-      carta.valor
-   })
-   
-
-} else {
-	console.log("O jogo acabou");
-}
+    if (confirm(`Quer iniciar uma rodada?`)){
+       // const usuario = []
+       // const computador = []
+       
+       // for (i = 0; i < 2; i++) {
+       //    usuario.push(comprarCarta())
+       //    computador.push(comprarCarta())
+       // }
+    
+       const usuario = [comprarCarta(), comprarCarta()]
+       const computador = [comprarCarta(), comprarCarta()]
+    
+       const somaUsuario = usuario[0].valor + usuario[1].valor
+       const somaComputador = computador[0].valor + computador[1].valor
+    
+       console.log(`Usuário - cartas: ${usuario[0].texto} e ${usuario[1].texto} - pontuação ${somaUsuario}`);
+       console.log(`Computador - cartas: ${computador[0].texto} e ${computador[1].texto} - pontuação ${somaComputador}`);
+    
+       if (somaUsuario > somaComputador){
+          console.log(`Usuário venceu`)
+       } else if (somaUsuario < somaComputador) {
+          console.log(`Computador venceu`)
+       } else {
+          console.log(`Empate!`)
+       }
+    
+    } else {
+       console.log(`Jogo cancelado`);
+    }
